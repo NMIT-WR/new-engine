1eddd6404de2b745e90beedc89dd064232a76cda
Petr Glaser <syreanis+1@gmail.com>
2024-12-13 00:33:45 +0100

    test: :white_check_mark: add tests with all option

--- apps/e2e/e2e/snapshots.spec.ts
+++ apps/e2e/e2e/snapshots.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
+import { testedEshops } from "../eshop_utils"
 
-test("landing matches snapshot", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveScreenshot();
-});
+for (const [eshop, url] of Object.entries(testedEshops)) {
+  test(`${eshop} landing matches snapshot`,async ({ page }) => {
+    await page.goto(`${url}/`);
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.15 });
+  });
+}
