3e6926c438ea735d31f506490df4ba6ba2b8a5b2
Petr Glaser <syreanis+1@gmail.com>
2024-12-13 01:05:40 +0100

    test: :art: improve test structure with baseUrl + dynamic name

--- apps/e2e/e2e/snapshots.spec.ts
+++ apps/e2e/e2e/snapshots.spec.ts
@@ -1,9 +1,13 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/test"
 import { testedEshops } from "../eshop_utils"
 
-for (const [eshop, url] of Object.entries(testedEshops)) {
-  test(`${eshop} landing matches snapshot`,async ({ page }) => {
-    await page.goto(`${url}/`);
-    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.15 });
-  });
+for (const [eshop, baseURL] of Object.entries(testedEshops)) {
+  test.describe(`${eshop} screenshot`, () => {
+    test.use({ baseURL })
+
+    test("landing matches snapshot", async ({ page }) => {
+      await page.goto("/")
+      await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.15 })
+    })
+  })
 }
