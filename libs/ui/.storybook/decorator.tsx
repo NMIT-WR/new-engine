15905901218af5810f62451cf6110348db413b1c
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-04-12 19:46:06 +0200

    refactor(tokens): add wr prefix

--- libs/ui/.storybook/decorator.tsx
+++ libs/ui/.storybook/decorator.tsx
@@ -11,12 +11,12 @@ export const VariantGroup: React.FC<VariantGroupProps> = ({
   children,
   fullWidth,
 }) => (
-  <div className="space-y-2 w-full">
-    <h3 className="font-semibold text-m text-fg-primary">{title}</h3>
+  <div className="wr:space-y-2 wr:w-full">
+    <h3 className="wr:font-semibold wr:text-m wr:text-fg-primary">{title}</h3>
     <div
-      className={`flex ${
-        fullWidth ? "flex-col" : "flex-wrap"
-      } gap-4 items-center`}
+      className={`wr:flex ${
+        fullWidth ? "wr:flex-col" : "wr:flex-wrap"
+      } wr:gap-4 wr:items-center`}
     >
       {children}
     </div>
@@ -25,4 +25,8 @@ export const VariantGroup: React.FC<VariantGroupProps> = ({
 
 export const VariantContainer: React.FC<{ children: ReactNode }> = ({
   children,
-}) => <div className="flex flex-col items-center space-y-8">{children}</div>;
+}) => (
+  <div className="wr:flex wr:flex-col wr:items-center wr:space-y-8">
+    {children}
+  </div>
+);
