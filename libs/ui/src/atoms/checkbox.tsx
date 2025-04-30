7ae228d5f967c7bb19b39857801855ccaa08289d
Kai Uwe <pisezdenekpise@seznam.cz>
2025-04-30 15:50:37 +0200

    refactor(ui): resolve css class collision for text-icon-md
    Renamed conflicting CSS class 'text-icon-md' in checkbox styles to 'text-checkbox-icon-md' to avoid collision with icon styles.

--- libs/ui/src/atoms/checkbox.tsx
+++ libs/ui/src/atoms/checkbox.tsx
@@ -9,15 +9,14 @@ const checkboxVariants = tv({
     "bg-checkbox checked:bg-checkbox-checked checked:border-checkbox",
     /* center icon, grid and place items do not work e.g. flex */
     "after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2",
-    "checked:after:token-icon-checkbox text-icon-text text-icon-md",
+    "checked:after:token-icon-checkbox text-icon-text text-checkbox-icon-md",
     "disabled:bg-checkbox-disabled disabled:border-checkbox-border-disabled disabled:cursor-not-allowed",
     "transition-all duration-200",
     "focus:outline-none focus-visible:ring-2 focus-visible:ring-checkbox-ring focus-visible:ring-offset-2",
   ],
 });
 
-export interface CheckboxProps
-  extends InputHTMLAttributes<HTMLInputElement> {
+export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   ref?: Ref<HTMLInputElement>;
 }
 
