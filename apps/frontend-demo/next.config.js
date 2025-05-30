a75de06f768ff58d2a50fdee4e18fe319ede4692
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-05-30 06:58:24 +0200

    fix(build): fix building process and deploy bugs

--- apps/frontend-demo/next.config.js
+++ apps/frontend-demo/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['ui'],
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig
