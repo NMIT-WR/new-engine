0e69c6e812121001e9d94cab3711a069a752d8e0
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-07-03 10:39:17 +0200

    refactor: category submenu

--- apps/frontend-demo/src/components/header-wrapper.tsx
+++ apps/frontend-demo/src/components/header-wrapper.tsx
@@ -15,18 +15,21 @@ interface HeaderWrapperProps {
 export function HeaderWrapper({ logo }: HeaderWrapperProps) {
   const navigationItems = useMemo<NavItem[]>(() => {
     const categoryItems: NavItem[] = [
-      { title: 'Všechny kategorie', href: '/categories' },
+      { title: 'Trika', href: '/products?categories=pcat_01JYERRCMBCA6DTA9D2QK47365,pcat_01JYERRCMZ5D5VXQNW13ZQ8B42' },
+      { title: 'Bundy', href: '/products?categories=pcat_01JYERRCR52228KG73ZTFJDFDH,pcat_01JYERRCRSY098ZP2TPDC8D8F3'},
+      { title: 'Kola', href: '/products?categories=pcat_01JYERRDV0ZKND1HNDAEATS1YN,pcat_01JYERRDVQDFDX10CCPPCYR06T,pcat_01JYERRDW8KWJ8D39PFQ3CEJVZ'},
+      { title: 'Doplǩy', href: '/products?categories=pcat_01JYERRF0F4T1VXMEEGDV9Z2VK,pcat_01JYERRF13W6SR9SNBCB543D5B,pcat_01JYERRF2XXRH863JMAC438GTD'}
     ]
 
     // Add static categories
-    if (allCategories.length > 0) {
+   /* if (allCategories.length > 0) {
       categoryItems.push(
         ...allCategories.slice(0, 10).map((cat) => ({
           title: cat.name,
           href: `/products?categories=${cat.id}`,
         }))
       )
-    }
+    }*/
 
     return [
       { title: 'Domů', href: '/', prefetch: false },
