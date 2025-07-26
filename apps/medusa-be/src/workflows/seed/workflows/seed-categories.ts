2449af00048049f4957751391f2e7da31a476569
Vojtech Dolezal <vojtechdolezalu@gmail.com>
2025-07-26 15:00:51 +0200

    feat: metadata import, stock quantities
    * refactored code to allow batching down the road

---
+++ apps/medusa-be/src/workflows/seed/workflows/seed-categories.ts
@@ -0,0 +1,39 @@
+import {createWorkflow, transform, WorkflowResponse} from "@medusajs/framework/workflows-sdk"
+import * as Steps from "../steps";
+
+export type CategoryRaw = {
+    title: string,
+    description?: string,
+    handle: string,
+    isActive: boolean,
+    parentHandle?: string
+}
+
+const seedCategoriesWorkflowId = 'seed-categories-workflow'
+const seedCategoriesWorkflow = createWorkflow(
+    seedCategoriesWorkflowId,
+    (input: CategoryRaw[]) => {
+
+        const productCategories: Steps.CreateProductCategoriesStepInput = transform({
+            input
+        }, (data) => {
+            return data.input.map(i => ({
+                name: i.title,
+                description: i.description,
+                handle: i.handle,
+                isActive: Boolean(Number(i.isActive)),
+                parentHandle: i.parentHandle,
+            }))
+        })
+
+        Steps.createProductCategoriesStep(productCategories)
+
+        return new WorkflowResponse({
+            result: {
+                message: 'Categories seeded successfully',
+            }
+        })
+    }
+)
+
+export default seedCategoriesWorkflow
