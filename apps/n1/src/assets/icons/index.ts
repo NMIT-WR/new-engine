b9c48c4d5118d183e80476c0ea6412e3f26d9b59
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-09-18 13:13:45 +0200

    init n1 app

---
+++ apps/n1/src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import carIcon from './car.png'
+import cardIcon from './card.png'
+import mapIcon from './map.png'
+import storeIcon from './store.png'
+
+export const icons = { carIcon, cardIcon, mapIcon, storeIcon }
