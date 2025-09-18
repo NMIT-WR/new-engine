be67dcd40c27bc9a38216be9a4299b2ef0138d5d
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-09-18 13:13:45 +0200

    init n1 app

---
+++ apps/n1/src/assets/top-category/index.ts
@@ -0,0 +1,11 @@
+import bestSellerImg from './bestseller.jpg'
+import bicularImg from './bicular.jpg'
+import electroImg from './electro.jpg'
+import tretryImg from './tretry.jpg'
+
+export const topCategoryImages = {
+  bestSellerImg,
+  bicularImg,
+  electroImg,
+  tretryImg,
+}
