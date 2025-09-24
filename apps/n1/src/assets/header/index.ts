4bb746614cb254860eaaa6071abfe1d36eb506c5
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-09-24 12:38:38 +0200

    feat(header,home): add header data and product badges/variants

---
+++ apps/n1/src/assets/header/index.ts
@@ -0,0 +1,136 @@
+// Pánské
+import panskeCyklo from './panske-cyklo.png'
+import panskeMoto from './panske-moto.png'
+import panskeObleceni from './panske-obleceni.png'
+import panskeSki from './panske-ski.jpg'
+import panskeSnbSkate from './panske-snb-skt.jpg'
+
+// Dámské
+import damskeCyklo from './damske-cyklo.png'
+import damskeMoto from './damske-moto.png'
+import damskeObleceni from './damske-obleceni.png'
+import damskeSki from './damske-ski.jpg'
+import damskeSnbSkate from './damske-snb-skate.jpg'
+
+// Dětské
+import detskeCyklo from './detske-cyklo.png'
+import detskeMoto from './detske-moto.jpg'
+import detskeObleceni from './detske-obleceni.jpg'
+import detskeSki from './detske-ski.jpg'
+import detskeSnbSkate from './detske-snb-skate.jpg'
+
+// Oblečení
+import obleceniBryle from './obleceni-bryle.jpg'
+import obleceniBundy from './obleceni-bundy.jpg'
+import obleceniKalhoty from './obleceni-kalhoty.jpg'
+import obleceniKosile from './obleceni-kosile.jpg'
+import obleceniKratasy from './obleceni-kratasy.jpg'
+import obleceniMikiny from './obleceni-mikiny.jpg'
+import obleceniPlavky from './obleceni-plavky.jpg'
+import obleceniSaty from './obleceni-saty.jpg'
+import obleceniDoplnky from './obleceni-soplnky.jpg'
+import obleceniSvetry from './obleceni-svetry.jpg'
+import obleceniTrika from './obleceni-trika.jpg'
+
+// Cyklo
+import cykloBoty from './cyklo-boty.jpg'
+import cykloChranice from './cyklo-chranice.jpg'
+import cykloDoplnky from './cyklo-doplnky.jpg'
+import cykloElektrokola from './cyklo-elektrokola.jpg'
+import cykloKola from './cyklo-kola.jpg'
+import cykloKomponenty from './cyklo-komponenty.jpg'
+import cykloNaradi from './cyklo-naradi.jpg'
+import cykloObleceni from './cyklo-obleceni.jpg'
+import cykloPreprava from './cyklo-preprava.jpg'
+import cykloPrilby from './cyklo-prilby.jpg'
+import cykloSedla from './cyklo-sedla.jpg'
+import cykloTasky from './cyklo-tasky.jpg'
+import cykloVyziva from './cyklo-vyziva.jpg'
+import cykloZapletenaKola from './cyklo-zapletena-kola.jpg'
+
+// Moto
+import motoBoty from './moto-boty.jpg'
+import motoBryle from './moto-bryle.jpg'
+import motoChranice from './moto-chranice.jpg'
+import motoDoplnky from './moto-doplnky.jpg'
+import motoObleceni from './moto-obleceni.jpg'
+import motoPrilby from './moto-prilby.jpg'
+
+// Snb-Skate
+import snbBrusle from './snb-brusle.jpg'
+import snbSkate from './snb-skate.jpg'
+import snbSnowboard from './snb-snowboard.jpg'
+
+// Ski
+import skiDoplnky from './ski-doplnky.jpg'
+import skiObleceni from './ski-obleceni.jpg'
+
+export const headerImgs = {
+  panske: {
+    cyklo: panskeCyklo,
+    moto: panskeMoto,
+    obleceni: panskeObleceni,
+    ski: panskeSki,
+    snbSkate: panskeSnbSkate,
+  },
+  damske: {
+    cyklo: damskeCyklo,
+    moto: damskeMoto,
+    obleceni: damskeObleceni,
+    ski: damskeSki,
+    snbSkate: damskeSnbSkate,
+  },
+  detske: {
+    cyklo: detskeCyklo,
+    moto: detskeMoto,
+    obleceni: detskeObleceni,
+    ski: detskeSki,
+    snbSkate: detskeSnbSkate,
+  },
+  obleceni: {
+    bryle: obleceniBryle,
+    bundy: obleceniBundy,
+    kalhoty: obleceniKalhoty,
+    kosile: obleceniKosile,
+    kratasy: obleceniKratasy,
+    mikiny: obleceniMikiny,
+    plavky: obleceniPlavky,
+    saty: obleceniSaty,
+    doplnky: obleceniDoplnky,
+    svetry: obleceniSvetry,
+    trika: obleceniTrika,
+  },
+  cyklo: {
+    boty: cykloBoty,
+    doplnky: cykloDoplnky,
+    elektrokola: cykloElektrokola,
+    chranice: cykloChranice,
+    kola: cykloKola,
+    komponenty: cykloKomponenty,
+    naradi: cykloNaradi,
+    obleceni: cykloObleceni,
+    preprava: cykloPreprava,
+    prilby: cykloPrilby,
+    sedla: cykloSedla,
+    tasky: cykloTasky,
+    vyziva: cykloVyziva,
+    zapletenaKola: cykloZapletenaKola,
+  },
+  moto: {
+    boty: motoBoty,
+    bryle: motoBryle,
+    doplnky: motoDoplnky,
+    chranice: motoChranice,
+    obleceni: motoObleceni,
+    prilby: motoPrilby,
+  },
+  snbSkate: {
+    brusle: snbBrusle,
+    skateboard: snbSkate,
+    snowboard: snbSnowboard,
+  },
+  ski: {
+    doplnky: skiDoplnky,
+    obleceni: skiObleceni,
+  },
+}
