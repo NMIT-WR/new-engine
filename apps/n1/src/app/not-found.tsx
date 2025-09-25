aa1a30b252f73de2ac607a4b922bc39dabc11d38
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-09-25 15:50:27 +0200

    feat(n1): add basic 404, error and loading pages

---
+++ apps/n1/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+export default function NotFound() {
+  return (
+    <main className="mx-auto grid w-max-w">
+      <article className="mt-900 space-y-400">
+        <h1 className="relative font-bold text-2xl after:absolute after:bottom-0 after:left-0 after:h-1 after:w-24 after:bg-primary">
+          Stránka nenalezena - 404
+        </h1>
+        <p className="text-fg-secondary">
+          Omlouváme se, ale tato stránka neexistuje. Co se zkusit vrátit na
+          hlavní stránku a prozkoumat náš eshop?
+        </p>
+      </article>
+    </main>
+  )
+}
