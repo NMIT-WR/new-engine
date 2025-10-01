1f268265c0fa1411b501b1f31eddcac7a8c80a6c
Kai Uwe <pisezdenekpise@seznam.cz>
2025-10-01 06:00:47 +0200

    refactor(categories): enhance data structure of categories

--- apps/n1/src/lib/constants.ts
+++ apps/n1/src/lib/constants.ts
@@ -1,12 +1,29 @@
+import { leafCategories, rootCategories } from "@/data/static/categories"
+
+export const ROOT_CATEGORIES = rootCategories.map((cat) => {
+  return {
+    id: cat.id,
+    handle: cat.handle
+  }
+})
+
+export const CATEGORIES_LEAFS_IDS = ROOT_CATEGORIES.map((cat) => {
+  const children = leafCategories.filter((leaf) => leaf.root_category_id === cat.id).map((leaf) => leaf.id)
+  return {
+    handle: cat.handle,
+    children
+  }
+})
+
 export const CATEGORY_MAP: Record<string, string[]> = {
-  panske: ['pcat_01K1RB8G2KVYFH1P049EASAHMN'],
-  damske: ['pcat_01K1RB8J9YCTA5RKQMSVVGMDS4'],
-  detske: ['pcat_01K1RB8M6Q6M3XTJ47TCCZ2E93'],
-  'obleceni-category-347': ['pcat_01K1RB8NDSY4KAVFFQVRNP2KAD'],
-  'cyklo-category-378': ['pcat_01K1RB8NWQRS644WV58YAVTTKV'],
-  'moto-category-424': ['pcat_01K1RB8PPDZVV74S77X81WB6XC'],
-  'snb-skate-category-448': ['pcat_01K1RB8PY751XV9DETPGD1VE0P'],
-  'ski-category-466': ['pcat_01K1RB8Q89WGTW50FKQVXAJBBT'],
+  panske: CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'panske')?.children || [],
+  damske: CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'damske')?.children || [],
+  detske: CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'detske')?.children || [],
+  'obleceni-category-347': CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'obleceni-category-347')?.children || [],
+  'cyklo-category-378': CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'cyklo-category-378')?.children || [],
+  'moto-category-424': CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'moto-category-424')?.children || [],
+  'snb-skate-category-448': CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'snb-skate-category-448')?.children || [],
+  'ski-category-466': CATEGORIES_LEAFS_IDS.find((cat) => cat.handle === 'ski-category-466')?.children || [],
 }
 
 export const PRODUCT_FIELDS = '*variants.calculated_price' as const
