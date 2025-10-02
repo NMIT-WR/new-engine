e79193a9a88b8e4f774cbf06d3a403bcde5ab332
Kai Uwe <pisezdenekpise@seznam.cz>
2025-10-02 06:58:39 +0200

    improve styling and props

--- apps/n1/src/components/atoms/banner.tsx
+++ apps/n1/src/components/atoms/banner.tsx
@@ -3,14 +3,18 @@ import type { ReactNode } from 'react'
 import type { VariantProps } from 'tailwind-variants'
 
 const bannerVariants = tv({
-  base: 'inline-flex',
+  base: 'inline-flex w-full border',
   variants: {
     variant: {
-      default: 'bg-banner-default-bg text-banner-default-fg',
-      info: 'bg-banner-info-bg text-banner-info-fg',
-      success: 'bg-banner-success-bg text-banner-success-fg',
-      warning: 'bg-banner-warning-bg text-banner-warning-fg',
-      danger: 'bg-banner-danger-bg text-banner-danger-fg',
+      default:
+        'bg-banner-default-bg text-banner-default-fg border-banner-default-border',
+      info: 'bg-banner-info-bg text-banner-info-fg border-banner-info-border',
+      success:
+        'bg-banner-success-bg text-banner-success-fg border-banner-success-border',
+      warning:
+        'bg-banner-warning-bg text-banner-warning-fg border-banner-warning-border',
+      danger:
+        'bg-banner-danger-bg text-banner-danger-fg border-banner-danger-border',
     },
     size: {
       sm: 'p-banner-sm text-banner-sm',
@@ -26,8 +30,13 @@ const bannerVariants = tv({
 
 interface BannerProps extends VariantProps<typeof bannerVariants> {
   children: ReactNode
+  className?: string
 }
 
-export function Banner({ children, variant, size }: BannerProps) {
-  return <div className={bannerVariants({ variant, size })}>{children}</div>
+export function Banner({ children, variant, size, className }: BannerProps) {
+  return (
+    <div className={bannerVariants({ variant, size, className })}>
+      {children}
+    </div>
+  )
 }
