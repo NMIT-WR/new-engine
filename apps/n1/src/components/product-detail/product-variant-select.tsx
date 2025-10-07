a63823078d189a804bdbcaa21caf737454bde126
Kai Uwe <pisezdenekpise@seznam.cz>
2025-10-07 19:27:45 +0200

    init detail product page and components

---
+++ apps/n1/src/components/product-detail/product-variant-select.tsx
@@ -0,0 +1,42 @@
+import type { ProductDetail, ProductVariantDetail } from '@/types/product'
+import { LinkButtonWithTooltip } from '../molecules/link-button-with-tooltip'
+import { TooltipContent } from './tooltip-content'
+
+interface ProductVariantSelectProps {
+  detail: ProductDetail
+  selectedVariant: ProductVariantDetail | null
+  handle: string
+}
+
+export const ProductVariantSelect = ({
+  detail,
+  selectedVariant,
+  handle,
+}: ProductVariantSelectProps) => {
+  return (
+    <div className="flex gap-200">
+      {detail.variants?.length > 1 &&
+        detail.variants.map((variant) => {
+          return (
+            <LinkButtonWithTooltip
+              href={`/produkt/${handle}?variant=${variant.title.toLowerCase()}`}
+              key={variant.id}
+              selected={variant.id === selectedVariant?.id}
+              tooltip={
+                <TooltipContent
+                  title={detail.title}
+                  variant={variant.title}
+                  quantity={variant.inventory_quantity ?? 0}
+                />
+              }
+              placement="bottom-end"
+              data-selected={variant.id === selectedVariant?.id}
+            >
+              {variant.title}
+              <span className="button-decoration" />
+            </LinkButtonWithTooltip>
+          )
+        })}
+    </div>
+  )
+}
---
+++ apps/n1/src/components/product-detail/store-status.tsx
@@ -0,0 +1,23 @@
+import { Tooltip } from '@new-engine/ui/atoms/tooltip'
+
+export const StoreStatus = ({ quantity }: { quantity: number }) => {
+  const StatusContent = () => (
+    <div className="text-md">
+      <h4 className="font-bold">Sklad N1shop - doba dodání 1-2 dny:</h4>
+      <p className="font-semibold text-success">{quantity} ks</p>
+    </div>
+  )
+
+  return (
+    <Tooltip
+      content={<StatusContent />}
+      className="bg-secondary text-fg-reverse [--arrow-background:var(--color-secondary)]"
+      placement="bottom-start"
+      offset={{ mainAxis: 4, crossAxis: 4 }}
+    >
+      <span className="cursor-help font-bold text-lg text-success underline decoration-1 decoration-dotted underline-offset-4">
+        Skladem {quantity} ks
+      </span>
+    </Tooltip>
+  )
+}
