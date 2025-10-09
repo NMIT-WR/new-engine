10fa06d82ab399ae3d0188a2aafd41d5b0638a35
Kai Uwe <pisezdenekpise@seznam.cz>
2025-10-09 10:16:37 +0200

    feat(product-info): add product info panel with variant, detail page UI/UX

---
+++ apps/n1/src/components/product-detail/product-info-panel/delivery-date.tsx
@@ -0,0 +1,37 @@
+import { useDate } from '@/hooks/use-date'
+import { Tooltip } from '@new-engine/ui/atoms/tooltip'
+import Link from 'next/link'
+
+export const DeliveryDate = () => {
+  const { addDays, day, short } = useDate()
+  const deliveryDate = addDays(3)
+
+  const tooltipContent = (
+    <article>
+      <Link href="/doprava-a-platba" className="font-bold underline">
+        PPL Doručení do výdejních míst
+      </Link>
+      <p>
+        Vyzvedněte si zásilku, kde je vám to blízké, v PPL Parcelboxu nebo PPL
+        Parcelshopu.
+      </p>
+    </article>
+  )
+  return (
+    <div className="flex items-center gap-150">
+      <Tooltip
+        content={tooltipContent}
+        placement="bottom-start"
+        className="max-w-2xs bg-secondary text-fg-reverse [--arrow-background:var(--color-secondary)]"
+      >
+        <span className="cursor-help border-2 border-success border-t-5 px-150 font-bold text-fg-secondary text-xl">
+          {day(deliveryDate)}
+        </span>
+      </Tooltip>
+      <div className="flex flex-col text-fg-secondary text-sm">
+        <span>Doručení do</span>
+        <span>{short(deliveryDate)}</span>
+      </div>
+    </div>
+  )
+}
