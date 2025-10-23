e1a426c7784cca618a0b3b5518a5e39c5d00cec8
Kai Uwe <pisezdenekpise@seznam.cz>
2025-10-23 11:53:39 +0200

    feat(templates): add carousel template
    - Create CarouselTemplate with props-based API
    - Support all Carousel variants and orientations
    - Add comprehensive Storybook stories

---
+++ libs/ui/src/templates/carousel.tsx
@@ -0,0 +1,91 @@
+import type { ElementType } from 'react'
+import type { IconType } from '../atoms/icon'
+import {
+  Carousel,
+  type CarouselRootProps,
+  type CarouselSlide,
+} from '../molecules/carousel'
+
+export interface CarouselTemplateProps<T extends ElementType = any>
+  extends Omit<CarouselRootProps<T>, 'children' | 'slideCount'> {
+  slides: CarouselSlide[]
+  showControls?: boolean
+  showIndicators?: boolean
+  showAutoplay?: boolean
+  prevIcon?: IconType
+  nextIcon?: IconType
+}
+
+export function CarouselTemplate<T extends ElementType = any>({
+  slides,
+  showControls = true,
+  showIndicators = true,
+  showAutoplay = false,
+  prevIcon = 'token-icon-carousel-prev',
+  nextIcon = 'token-icon-carousel-next',
+  size = 'md',
+  objectFit = 'cover',
+  aspectRatio = 'square',
+  orientation = 'horizontal',
+  loop = true,
+  autoplay = false,
+  allowMouseDrag = true,
+  slidesPerPage = 1,
+  slidesPerMove = 1,
+  spacing = '0px',
+  padding = '0px',
+  imageAs,
+  width,
+  height,
+  className,
+  onPageChange,
+  ...carouselProps
+}: CarouselTemplateProps<T>) {
+  return (
+    <Carousel
+      size={size}
+      objectFit={objectFit}
+      aspectRatio={aspectRatio}
+      orientation={orientation}
+      slideCount={slides.length}
+      loop={loop}
+      autoplay={autoplay}
+      allowMouseDrag={allowMouseDrag}
+      slidesPerPage={slidesPerPage}
+      slidesPerMove={slidesPerMove}
+      spacing={spacing}
+      padding={padding}
+      imageAs={imageAs}
+      width={width}
+      height={height}
+      className={className}
+      onPageChange={onPageChange}
+      {...carouselProps}
+    >
+      <Carousel.Slides
+        slides={slides}
+        imageAs={imageAs}
+        width={width}
+        height={height}
+      />
+
+      {(showControls || showIndicators) && (
+        <Carousel.Control>
+          {showControls && <Carousel.Previous icon={prevIcon} />}
+          {showControls && showIndicators && <div className="flex-1" />}
+          {showIndicators && (
+            <Carousel.Indicators>
+              {slides.map((_, index) => (
+                <Carousel.Indicator key={index} index={index} />
+              ))}
+            </Carousel.Indicators>
+          )}
+          {showControls && showIndicators && <div className="flex-1" />}
+          {showControls && <Carousel.Next icon={nextIcon} />}
+        </Carousel.Control>
+      )}
+
+      {showAutoplay && <Carousel.Autoplay />}
+    </Carousel>
+  )
+}
