3c01a6fd574feb804c6098a9f1306eb2728c90f2
KaiUweCZE <pisezdenekpise@seznam.cz>
2025-10-31 11:23:30 +0100

    feat(header): rewrite default grid by flex utilities in Header.Container and buttons to fix alignment issues

--- apps/n1/src/components/header/n1-header.tsx
+++ apps/n1/src/components/header/n1-header.tsx
@@ -47,7 +47,7 @@ export const N1Header = () => {
       direction="vertical"
       className="z-50 flex h-fit max-h-96 w-full flex-col"
     >
-      <Header.Container className="items-center justify-between bg-highlight px-400 py-150">
+      <Header.Container className="flex items-center justify-between bg-highlight px-400 py-150">
         <div className="flex items-center gap-200 font-normal text-[12.6px] text-fg-reverse">
           <Link as={NextLink} href="mailto:office@n1shop.cz">
             <Icon icon="icon-[mdi--email-outline]" className="mr-200" />
@@ -72,7 +72,7 @@ export const N1Header = () => {
           <Icon icon="icon-[cif--gb]" />
         </div>
       </Header.Container>
-      <Header.Container className="z-40 justify-between bg-gray-950 px-500 py-300">
+      <Header.Container className="z-40 flex justify-between bg-gray-950 px-500 py-300">
         <div className="flex h-full items-center gap-750">
           <Image
             src={footerImgs.footerLogo}
@@ -97,7 +97,7 @@ export const N1Header = () => {
               variant="primary"
               size="current"
               href={button.href}
-              className="gap-x-300 px-0 py-0 text-2xl text-fg-reverse hover:bg-transparent hover:text-primary"
+              className="items-center gap-x-300 px-0 py-0 text-2xl text-fg-reverse hover:bg-transparent hover:text-primary"
             >
               {<Icon icon={button.icon as IconType} />}
               {button.children && (
