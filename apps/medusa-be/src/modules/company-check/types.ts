d94c82ffa79de047865b154e0a63f34aceddb7a6
David Uhrin <daviduhrin98@gmail.com>
2026-02-08 22:49:56 +0100

    Add Vies client.

--- apps/medusa-be/src/modules/company-check/types.ts
+++ apps/medusa-be/src/modules/company-check/types.ts
@@ -1,3 +1,6 @@
+import { z } from "zod"
+import { ViesCheckVatResponseSchema } from "./schema"
+
 export type CompanyInfo = {
   company_name: string
   company_identification_number: string
@@ -6,4 +9,15 @@ export type CompanyInfo = {
   city: string
   country: string
   postal_code: string
+}
+
+export type ViesCheckVatRequest = {
+  countryCode: string
+  vatNumber: string
+}
+
+export type ViesCheckVatResponse = z.infer<typeof ViesCheckVatResponseSchema>
+
+export type ViesClientOptions = {
+  baseUrl: string
 }
